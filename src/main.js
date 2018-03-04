9668c43f944b34e638dea9055c9e687d90c4d501
Gigacore <santhoshinbang@gmail.com>
2018-03-04 14:09:21 +0530

    reset matrix

--- src/main.js
+++ src/main.js
@@ -1,26 +1,26 @@
-// const matrix = [
-//   [5,3,null,null,7,null,null,null,null],
-//   [6,null,null,1,9,5,null,null,null],
-//   [1,9,8,null,null,null,null,6,null],
-//   [8,null,null,null,6,null,null,null,3],
-//   [4,null,null,8,null,3,null,null,1],
-//   [7,null,null,null,2,null,null,null,6],
-//   [null,6,null,null,null,null,2,8,null],
-//   [null,null,null,4,1,9,null,null,5],
-//   [null,null,null,null,8,null,null,7,9]
-// ];
-
 const matrix = [
-  [2,9,5,7,4,3,8,6,1],
-  [4,3,1,8,6,5,9,2,7],
-  [8,7,6,1,9,2,5,4,3],
-  [3,8,7,4,5,9,2,1,6],
-  [6,1,2,3,8,7,4,9,5],
-  [5,4,9,2,1,6,7,3,8],
-  [7,6,3,5,3,4,1,8,9],
-  [9,2,8,6,7,1,3,5,4],
-  [1,5,4,9,3,8,6,7,null]
+  [5,3,null,null,7,null,null,null,null],
+  [6,null,null,1,9,5,null,null,null],
+  [1,9,8,null,null,null,null,6,null],
+  [8,null,null,null,6,null,null,null,3],
+  [4,null,null,8,null,3,null,null,1],
+  [7,null,null,null,2,null,null,null,6],
+  [null,6,null,null,null,null,2,8,null],
+  [null,null,null,4,1,9,null,null,5],
+  [null,null,null,null,8,null,null,7,9]
 ];
+
+// const matrix = [
+//   [2,9,5,7,4,3,8,6,1],
+//   [4,3,1,8,6,5,9,2,7],
+//   [8,7,6,1,9,2,5,4,3],
+//   [3,8,7,4,5,9,2,1,6],
+//   [6,1,2,3,8,7,4,9,5],
+//   [5,4,9,2,1,6,7,3,8],
+//   [7,6,3,5,3,4,1,8,9],
+//   [9,2,8,6,7,1,3,5,4],
+//   [1,5,4,9,3,8,6,7,null]
+// ];
 
 const matrixLength = 3;
 
